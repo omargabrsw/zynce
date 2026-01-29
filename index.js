1e7294e8dfc540d7c5aaaeb7a43e31cd2c63a83c
omargabrsw <omargabrsw@gmail.com>
2026-01-29 02:06:33 +0200

    Refactor index.js to improve database connection handling and task retrieval logic

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+// dotenv Import
 import 'dotenv/config';
+// mysql import
 import mysql from 'mysql';
-import http from 'node:http';
-http.createServer();
+// HTTP NodeJS Module import
+import * as http from 'node:http';
+// Make Web Server
+const server = http.createServer();
+// Make Database Connection
 const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
 });
-connection.connect((err) => {
-  if (err) {
-    console.log('error while connecting' + err.stack);
+
+// Make DB Connection
+
+async function getTasks() {
+  const query = 'select * from tasks';
+
+  return new Promise((res, rej) => {
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.log(error);
+        return rej(error);
+      }
+      return res(results);
+    });
+  });
+}
+let tasks = await getTasks();
+
+server.on('request', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  if (req.method === 'GET' && req.url === '/api/tasks') {
+    res.writeHead(200, 'The API is working now bruv');
+    res.end(JSON.stringify(tasks));
+  } else {
+    res.writeHead(404, 'Not Found');
   }
 });
 
-connection.query('select * from tasks', async (err, res, fields) => {
-  if (err) console.log('error');
-  const taskName = res[0].task_name;
-  await console.log(taskName);
-});
-
-connection.end();
+server.listen(8000, '127.0.0.1');
