a5fbbfa66d1e2bbe98d68786dfd52a9f3df7971d
omargabrsw <omargabrsw@gmail.com>
2026-02-05 07:17:40 +0200

    error Fixed

--- server/index.js
+++ server/index.js
@@ -1,41 +1,14 @@
-// dotenv Import
-import "dotenv/config";
-// mysql import
-import mysql from "mysql";
 // HTTP NodeJS Module import
+
 import * as http from "node:http";
+
+import { handleRoutes } from "./routes/taskRoutes";
+
 // Make Web Server
-const server = http.createServer();
-// Make Database Connection
-const connection = mysql.createPoolCluster({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
 
-async function getTasks() {
-  const query = "select * from tasks";
+const server = http.createServer(
 
-  return new Promise((res, rej) => {
-    connection.query(query, (error, results) => {
-      if (error) {
-        console.log(error);
-        return rej(error);
-      }
-      return res(results);
-    });
-  });
-}
-server.on("request", async (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  if (req.method === "GET" && req.url === "/api/tasks") {
-    let tasks = await getTasks();
-    res.statusCode = 200;
-    res.statusMessage = "Code working bro don't worry trust the chad";
-    res.end(JSON.stringify(tasks));
-  } else {
-    res.writeHead(404, "Not Found");
-  }
-});
+server.on('request',handleRoutes(req,res)));
+
 server.listen(8000, "127.0.0.1");
+
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,2 +1,11 @@
 //Import taskController
 import {getTasks} from "../controllers/taskController.js";
+
+export function handleRoutes(req,res){
+  //Get Tasks
+  if (req.method === "GET" && req.url === "/api/tasks") {
+    return getTasks(req,res);
+  }
+
+    res.writeHead(404, "Not Found");
+}
